8384d9bab5a95c5594e75f7478b16c548c6d4497
dariusz zielonka <dariuszzielonka727@gmail.com>
2023-12-03 13:00:01 +0100

    slight cleanup

--- lib/mix/tasks/aoc3.ex
+++ lib/mix/tasks/aoc3.ex
@@ -9,7 +9,10 @@ defmodule Mix.Tasks.Aoc3 do
     IO.puts "Part 2: #{part2(file)}"
   end
 
-  def adjacent({char_x, char_y}, {start_x, y, end_x}) do
+  def adjacent?({char_x, char_y}, {{num_x, num_y}, num_val}) do
+    {start_x, y} = {num_x, num_y}
+    end_x = start_x + String.length(num_val) - 1
+
     abs(y - char_y) <= 1
     && char_x >= start_x - 1
     && char_x <= end_x + 1
@@ -19,20 +22,13 @@ defmodule Mix.Tasks.Aoc3 do
       {numbers, special_chars} = parse(filestream)
 
       sum = numbers
-        |> Enum.filter(fn {num_key, num_val} ->
-          {start_x, y} = num_key
-          end_x = start_x + String.length(num_val) - 1
-
+        |> Enum.filter(fn number_kv ->
           special_chars
             |> Enum.any?(fn {char_key, _} ->
-              {char_x, char_y} = char_key
-
-              adjacent({char_x, char_y}, {start_x, y, end_x})
+              adjacent?(char_key, number_kv)
             end)
         end)
-        |> Enum.map(fn {_, num_val} ->
-          String.to_integer(num_val)
-        end)
+        |> Enum.map(fn {_, num_val} -> String.to_integer(num_val) end)
         |> Enum.sum()
 
       sum
@@ -45,10 +41,8 @@ defmodule Mix.Tasks.Aoc3 do
       |> Enum.filter(fn {_, char} -> char == "*" end)
       |> Enum.map(fn {char_key, _} ->
         adjacent_nums = numbers
-          |> Enum.filter(fn {num_key, num_val} ->
-            {start_x, y} = num_key
-            end_x = start_x + String.length(num_val) - 1
-            adjacent(char_key, {start_x, y, end_x})
+          |> Enum.filter(fn number_kv ->
+            adjacent?(char_key, number_kv)
           end)
           |> Enum.map(fn {_, num_val} -> String.to_integer(num_val) end)
 
@@ -60,31 +54,37 @@ defmodule Mix.Tasks.Aoc3 do
       |> Enum.sum()
   end
 
+  @digits ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
   def parse(filestream) do
-    roughly_parsed = filestream
+    trimmed_with_index = filestream
       |> Enum.map(&String.trim/1)
       |> Enum.with_index()
 
-    special_chars = roughly_parsed
+    special_chars = trimmed_with_index
       |> Enum.reduce(%{}, fn {line, y}, acc ->
-        String.codepoints(line)
+        line
+          |> String.codepoints()
           |> Enum.with_index()
-          |> Enum.reject(fn {char, _} -> char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."] end)
+          |> Enum.reject(fn {char, _} -> char in @digits or char == "." end)
           |> Enum.reduce(acc, fn {char, x}, line_acc ->
             Map.put(line_acc, {x, y}, char)
           end)
       end)
 
-    numbers = roughly_parsed
-      |> Enum.reduce(%{}, fn {line, y}, acc ->
-        res = String.codepoints(line <> ".")
+    numbers = trimmed_with_index
+      |> Enum.reduce(%{}, fn {line, y}, map ->
+        res = line <> "."
+          |> String.codepoints()
           |> Enum.with_index()
-          |> Enum.reduce({acc, {nil, ""}}, fn {char, x}, {line_acc, {initial_x, num}} ->
-            case char in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] do
-              true when initial_x == nil -> {line_acc, {x, num <> char}}
-              true -> {line_acc, {initial_x, num <> char}}
-              false when num != "" -> {Map.put(line_acc, {initial_x, y}, num), {nil, ""}}
-              false -> {line_acc, {initial_x, num}}
+          |> Enum.reduce({map, {0, ""}}, fn {char, x}, {line_map, {initial_x, num}} ->
+            case char in @digits do
+              true when initial_x == 0 -> {line_map, {x, num <> char}}
+              true -> {line_map, {initial_x, num <> char}}
+              false when num != "" ->
+                line_map = Map.put(line_map, {initial_x, y}, num)
+                {line_map, {0, ""}}
+              false -> {line_map, {initial_x, num}}
             end
           end)
 
