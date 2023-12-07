04fd9b3bbd8217c25d4f2e86d9d6633075862c55
dariusz zielonka <dariuszzielonka727@gmail.com>
2023-12-07 21:02:09 +0100

    cleanup

--- lib/mix/tasks/aoc7.ex
+++ lib/mix/tasks/aoc7.ex
@@ -5,16 +5,16 @@ defmodule Mix.Tasks.Aoc7 do
     path = elem(File.cwd, 1) <> "/inputs/input7.txt"
     file = File.stream!(path)
 
-    {p1time, p1} = :timer.tc(&part1/1, [file])
+    {p1time, p1} = :timer.tc(&solution/2, [file, &parse_hand1/1])
     IO.puts "Part 1: #{p1} - took #{p1time/1000}ms"
-    {p2time, p2} = :timer.tc(&part2/1, [file])
+    {p2time, p2} = :timer.tc(&solution/2, [file, &parse_hand2/1])
     IO.puts "Part 2: #{p2} - took #{p2time/1000}ms"
   end
 
-  def part2(filestream) do
-    hands = filestream
+  def solution(filestream, parse_hand) do
+    filestream
       |> Enum.map(&String.split(&1, " "))
-      |> Enum.map(fn [hand, bid] -> [parse_hand2(String.trim(hand)), String.to_integer(String.trim(bid))] end)
+      |> Enum.map(fn [hand, bid] -> [parse_hand.(String.trim(hand)), String.to_integer(String.trim(bid))] end)
       |> Enum.reduce([], fn [{type, hand}, bid], ranked_cards ->
         rank = ranked_cards
           |> Enum.reduce(0, fn [{cmp_type, cmp_hand}, cmp_bid], rank ->
@@ -77,39 +77,7 @@ defmodule Mix.Tasks.Aoc7 do
     end
 
     type = possible_hands
-      |> Enum.map(fn hand ->
-        highest_duplicate_counts = hand
-          |> Enum.frequencies()
-          |> Map.values()
-          |> Enum.reduce([], fn val, acc ->
-            curr_highest = Enum.at(acc, 0)
-            case val do
-              _ when val > curr_highest -> [val]
-              _ when val == curr_highest -> [val | acc]
-              _ when acc == [] -> [val]
-              _ -> acc
-            end
-          end)
-
-        type = case highest_duplicate_counts do
-          [1, 1, 1, 1, 1] -> 0
-          [2] -> 1
-          [2, 2] -> 2
-
-          [3] -> 3
-          [4] -> 5
-          [5] -> 6
-        end
-
-        case hand
-          |> Enum.frequencies()
-          |> Map.values()
-          |> Enum.sort
-        do
-          [2, 3] -> 4
-          _ -> type
-        end
-      end)
+      |> Enum.map(fn hand -> get_hand_type(hand) end)
       |> Enum.max
 
     {type, original_hand}
@@ -129,70 +97,26 @@ defmodule Mix.Tasks.Aoc7 do
         end
       end)
 
-    highest_duplicate_counts = parsed_hand
-      |> Enum.frequencies()
-      |> Map.values()
-      |> Enum.reduce([], fn val, acc ->
-        curr_highest = Enum.at(acc, 0)
-        case val do
-          _ when val > curr_highest -> [val]
-          _ when val == curr_highest -> [val | acc]
-          _ when acc == [] -> [val]
-          _ -> acc
-        end
-      end)
-
-    type = case highest_duplicate_counts do
-      [1, 1, 1, 1, 1] -> 0
-      [2] -> 1
-      [2, 2] -> 2
-
-      [3] -> 3
-      [4] -> 5
-      [5] -> 6
-    end
-
-    type = case parsed_hand
-      |> Enum.frequencies()
-      |> Map.values()
-      |> Enum.sort
-    do
-      [2, 3] -> 4
-      _ -> type
-    end
+    type = get_hand_type(parsed_hand)
 
     {type, parsed_hand}
   end
 
-  def part1(filestream) do
-    hands = filestream
-      |> Enum.map(&String.split(&1, " "))
-      |> Enum.map(fn [hand, bid] -> [parse_hand1(String.trim(hand)), String.to_integer(String.trim(bid))] end)
-      |> Enum.reduce([], fn [{type, hand}, bid], ranked_cards ->
-        rank = ranked_cards
-          |> Enum.reduce(0, fn [{cmp_type, cmp_hand}, cmp_bid], rank ->
-            case {type, cmp_type} do
-              {_, _} when type > cmp_type -> rank + 1
-              {_, _} when type < cmp_type -> rank
-              {_, _} when type == cmp_type ->
-                hand
-                  |> Enum.zip(cmp_hand)
-                  |> Enum.reduce_while(rank, fn {card, cmp_card}, acc ->
-                    case {card, cmp_card} do
-                      {_, _} when card > cmp_card -> {:halt, acc + 1}
-                      {_, _} when card < cmp_card -> {:halt, acc}
-                      {_, _} when card == cmp_card -> {:cont, acc}
-                    end
-                  end)
-            end
-          end)
+  def get_hand_type(parsed_hand) do
+    highest_duplicate_counts = parsed_hand
+      |> Enum.frequencies()
+      |> Map.values()
+      |> Enum.sort()
+      |> Enum.reverse()
 
-        List.insert_at(ranked_cards, rank, [{type, hand}, bid])
-      end)
-      |> Enum.with_index()
-      |> Enum.reduce(0, fn {[{type, hand}, bid], rank}, acc ->
-        acc + (rank + 1) * bid
-      end)
+    case highest_duplicate_counts do
+      [5 | _] -> 6
+      [4 | _] -> 5
+      [3, 2 | _] -> 4
+      [3 | _] -> 3
+      [2, 2 | _] -> 2
+      [2 | _] -> 1
+      [1, 1, 1, 1, 1] -> 0
+    end
   end
-
 end
